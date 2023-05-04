a67d789314b7b3343a69297accee22b5f9587a23
slimpotatoboy <edwinmhzn@gmail.com>
2023-05-04 15:09:35 +0545

    homepage setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ig_clone/screens/home_screen.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +10,12 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp(
+      title: "Instagram",
+      theme: ThemeData(
+        useMaterial3: true,
       ),
+      home: HomeScreen(),
     );
   }
 }
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int currentPageIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: SvgPicture.asset("assets/images/instagram.svg"),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.chat_outlined),
+          )
+        ],
+      ),
+      body: SafeArea(
+        child: <Widget>[
+          Container(
+            color: Colors.white,
+            alignment: Alignment.center,
+            child: const Text('Page 1'),
+          ),
+          Container(
+            color: Colors.green,
+            alignment: Alignment.center,
+            child: const Text('Page 2'),
+          ),
+          Container(
+            color: Colors.blue,
+            alignment: Alignment.center,
+            child: const Text('Page 3'),
+          ),
+          Container(
+            color: Colors.purple,
+            alignment: Alignment.center,
+            child: const Text('Page 4'),
+          ),
+          Container(
+            color: Colors.amber,
+            alignment: Alignment.center,
+            child: const Text('Page 5'),
+          ),
+        ][currentPageIndex],
+      ),
+      bottomNavigationBar: NavigationBar(
+        onDestinationSelected: (int index) {
+          setState(() {
+            currentPageIndex = index;
+          });
+        },
+        selectedIndex: currentPageIndex,
+        destinations: const <Widget>[
+          NavigationDestination(
+            icon: Icon(Icons.home),
+            label: '',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.search),
+            label: '',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.add),
+            label: '',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.video_call),
+            label: '',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.person),
+            label: '',
+          ),
+        ],
+      ),
+    );
+  }
+}
